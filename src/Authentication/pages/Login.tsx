80bfc1116198c5b802da65b7e9684ca6ef57faa3
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-19 17:43:21 +0300

    Refactor Login page styling

--- src/Authentication/pages/Login.tsx
+++ src/Authentication/pages/Login.tsx
@@ -1,6 +1,6 @@
 export const Login = () => {
   return (
-    <div className="bg-sky-100 flex justify-center items-center h-screen">
+    <div className="bg-beige flex justify-center items-center h-screen">
       <div className="w-1/2 h-screen hidden lg:block">
         <img
           src="https://imgs.search.brave.com/cJ8Sv_AvFN4U4Fe1q72njil-6NPRNdtGN_zijlQLtng/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTAw/NTIxNjUzMi9waG90/by9vbmxpbmUtYXVj/dGlvbi1jb25jZXB0/LndlYnA_Yj0xJnM9/NjEyeDYxMiZ3PTAm/az0yMCZjPW9KWl9R/SmNCNjBQQ0FjcnJo/Qk5zT2tWNmxiZnVK/RHRWYVhfdGZoRUpi/SFE9"
@@ -19,11 +19,6 @@ export const Login = () => {
         >
           Login
         </button>
-        <div className="mt-6 text-green-500 text-center">
-          <a href="#" className="hover:underline">
-            Sign up Here
-          </a>
-        </div>
       </div>
     </div>
   );
