244d2ecea6f8cc968d5c4cd4060fbc008e279244
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-19 21:18:40 +0300

    Refactor project structure and update .gitignore

--- SERVER/src/routes/smsRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { sendSMS, verifySMS } from "../controllers/smsController";
-
-const router = Router();
-
-router.post("/send-sms", sendSMS);
-router.post("/verify-sms", verifySMS);
-
-export default router;
