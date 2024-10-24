1af9d712015024f5bc529bbb4737e8adc75061a7
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-24 22:29:55 +0300

    Refactor RightBar component to display user profile photo and name

--- CLIENT/src/Room/pages/view/RightBar.tsx
+++ CLIENT/src/Room/pages/view/RightBar.tsx
@@ -1,5 +1,16 @@
+import { useAuth } from "../../../Context";
+
 export const RightBar = () => {
+  const { profile } = useAuth();
+  console.log(profile)
   return (
-    <div className="col-span-1">RightBar</div>
-  )
-}
+    <div className="col-span-1 bg-secondaryBackground sticky top-12 p-8 rounded-xl">
+      <div>
+        <img src={profile.photoURL} className="w-96 h-96" alt="" />
+        <p>
+            {profile.displayName}
+        </p>
+      </div>
+    </div>
+  );
+};
