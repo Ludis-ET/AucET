6a4e3b85a1d88f22bd31da4745d6946cc5a0ccb3
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-27 10:04:39 +0300

    Refactor Home component to include Footer and improve layout

--- CLIENT/src/Home/Home.tsx
+++ CLIENT/src/Home/Home.tsx
@@ -1,9 +1,13 @@
-import { Header } from "./components";
+import { Footer, Header } from "./components";
 
 const Home = () => {
   return (
     <div className="bg-mainBackground min-h-screen overflow-y-scroll p-8">
       <Header />
+      <main className="min-h-[80vh]">
+
+      </main>
+      <Footer />
     </div>
   );
 };
