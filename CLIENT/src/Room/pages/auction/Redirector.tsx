14855f9978396ddd60ec7e8e352c0b480993b06e
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-27 21:19:07 +0300

    Refactor Redirector component to fetch and display room details

--- CLIENT/src/Room/pages/auction/Redirector.tsx
+++ CLIENT/src/Room/pages/auction/Redirector.tsx
@@ -1,7 +1,45 @@
+import { useParams } from "react-router-dom";
+import { getRoomById, RoomType } from "../../requests";
+import { CountDown, Loader } from "../../components";
+import { Timestamp } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
 export const Redirector = () => {
+  const { roomId } = useParams();
+  const [room, setRoom] = useState<RoomType | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchRoom = async () => {
+      setLoading(true);
+      if (!roomId) {
+        setLoading(false);
+        return;
+      }
+      const response = await getRoomById(roomId);
+      setRoom(response);
+      setLoading(false);
+    };
+
+    fetchRoom();
+  }, [roomId]);
+
+  if (loading || !room) return <Loader text="Fetching room details..." />;
+  const check = (val: string | Timestamp) => typeof val === "string";
+
   return (
-    <div className="bg-mainBackground min-h-screen p-8">
-      Redirector
+    <div className="bg-mainBackground min-h-screen p-8 flex justify-center">
+      <div className="bg-secondaryBackground p-2 min-w-[40vw] max-h-[80vh] rounded-xl shadow-2xl">
+        <header>
+          <CountDown
+            time={
+              check(room.newFormValues.startdate)
+                ? room.newFormValues.startdate
+                : ""
+            }
+          />
+        </header>
+      </div>
     </div>
   );
 };
